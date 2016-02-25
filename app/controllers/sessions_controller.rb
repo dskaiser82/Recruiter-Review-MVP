558c697f8bc1e7a8b9d4ebf892f3d1d5b8642d6d
Dan Kaiser <dskaiser82@gmail.com>
2016-02-24 16:50:40 -0800

    re-addeed +REview button agency index

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   if @user && @user.authenticate(params[:password])
     session[:user_id] = @user.id
     flash[:success] = "Welcome! Thanks for Logging-in!"
-    redirect_to user_path(@user)
+    redirect_to agencies_path
   else
     flash[:failure] = "Oops. Something went wrong. Try again."
     redirect_to new_session_path
