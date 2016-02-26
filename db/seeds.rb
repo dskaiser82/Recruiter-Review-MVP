8d86cac9c5abff469bd94756ef33c4e8a684b55b
Dan Kaiser <dskaiser82@gmail.com>
2016-02-25 16:56:15 -0800

    Added blue button for reviews, bigger stars

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)Tweet.destroy.
+
+if User.destroy_all && Agency.destroy_all
+
+end
