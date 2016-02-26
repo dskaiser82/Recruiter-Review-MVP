8b964389bfd5b35b99629482b704a7176585d182
Dan Kaiser <dskaiser82@gmail.com>
2016-02-26 09:14:28 -0800

    testing seeds file on heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)Tweet.destroy.
 
 if User.destroy_all && Agency.destroy_all
-
+  User.create({name: "Dan", email: "dan@gmail.com", password: "1234"})
+  Agency.create({name: "Vitamin Talent", website: "http://vitamintalent.com", headquarters: "Boston,MA", focus: "Creative Staffing",  logo: "1-logo.png", about:"Vitamin T connects amazing digital creative talent with awesome mid-sized companies and ad agencies - and we guarantee our matches 110%."})
+  Agency.first.reviews.create({rating: 5, title: "This is a review", body: "The body for the review!", user: User.first})
 end
